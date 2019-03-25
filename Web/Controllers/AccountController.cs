eb41a583ce5f4be97801f8830d140665a2af1bf8
Stefan Dimitrov <s.djimitrov@gmail.com>
2019-03-25 19:36:35 +0200

    Added login initial code

---
+++ Web/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models.Account;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Web/Models/Account/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Models.Account
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
