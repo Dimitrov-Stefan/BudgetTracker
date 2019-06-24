256e3dc81eb6a2ee212e9be2dd8f05bb308f0ece
Dimitrov-Stefan <s.djimitrov@gmail.com>
2019-06-24 16:13:14 +0300

    Started working on financial item search typeahead

---
+++ Web/Models/Common/FinancialItemSearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Web.Models.Common
+{
+    public class FinancialItemSearchViewModel
+    {
+        public IDictionary<int, string> FinancialItems { get; set; }
+    }
+}
---
+++ Web/ViewComponents/FinancialItemSearchViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Core.Contracts.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Extensions;
+using System.Security.Claims;
+
+namespace Web.ViewComponents
+{
+    public class FinancialItemSearchViewComponent : ViewComponent
+    {
+        private readonly IFinancialItemsService _financialItemsService;
+
+        public FinancialItemSearchViewComponent(IFinancialItemsService financialItemsService)
+        {
+            _financialItemsService = financialItemsService;
+        }
+
+        public async IViewComponentResult Invoke()
+        {
+            var id = HttpContext.User.GetCurrentUserId();
+
+            var items = await _financialItemsService.GetAllByUserIdAsync(id);
+            //var itemsDict = items.Select(fi => new { Id = fi.Id, Name = fi.Name }).ToDictionary(x => { key});
+
+            return View();
+        }
+    }
+}
