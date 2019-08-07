8448895ba2642ec2b748b5f10621f0776cc8ed51
Dimitrov-Stefan <s.djimitrov@gmail.com>
2019-08-07 15:10:26 +0300

    Added paging models

---
+++ Models/PagedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Models
+{
+    public class PagedList<T>
+    {
+        #region Constructors
+
+        public PagedList(IEnumerable<T> items, int page, int pageSize, int totalItemCount)
+        {
+            Items = items ?? Enumerable.Empty<T>();
+            Page = page;
+            PageSize = pageSize;
+            TotalItemCount = totalItemCount;
+            PageCount = TotalItemCount > 0
+                ? (int)Math.Ceiling(TotalItemCount / (double)PageSize)
+                : 0;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public IEnumerable<T> Items { get; private set; }
+
+        public int Page { get; private set; }
+
+        public int PageSize { get; private set; }
+
+        public int TotalItemCount { get; private set; }
+
+        public int PageCount { get; private set; }
+
+        #endregion
+    }
+}
---
+++ Models/PagedListRequest.cs
@@ -0,0 +1,44 @@
+﻿namespace Models
+{
+    /// <summary>
+    /// A class that represents a paged list request.
+    /// </summary>
+    public class PagedListRequest
+    {
+        #region Fields
+
+        private const int DefaultPageSize = 10;
+
+        private int _page = 1;
+        private int _pageSize = DefaultPageSize;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the page.
+        /// </summary>
+        public int Page
+        {
+            get => _page;
+            set => _page = value > 0 ? value : 1;
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the page.
+        /// </summary>
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > 0 ? value : DefaultPageSize;
+        }
+
+        /// <summary>
+        /// Gets the skip.
+        /// </summary>
+        public int Skip => (Page - 1) * PageSize;
+
+        #endregion
+    }
+}
