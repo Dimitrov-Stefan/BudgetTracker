597411f29e6d12af3cf7c70effb98a4890dc110d
Dimitrov-Stefan <s.djimitrov@gmail.com>
2019-08-07 11:45:20 +0300

    Added paging view component

---
+++ Web/Models/Common/PaginationModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models.Common
+{
+    public class PaginationModel
+    {
+        /// <summary>
+        /// Gets or sets the function for generating the page URL.
+        /// </summary>
+        public Func<int, string> PageUrlFunc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current page.
+        /// </summary>
+        public int CurrentPage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total number of pages.
+        /// </summary>
+        public int PageCount { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the previous page should be shown.
+        /// </summary>
+        public bool HasPreviousPage => CurrentPage > 1;
+
+        /// <summary>
+        /// Gets a value indicating whether the next page should be shown.
+        /// </summary>
+        public bool HasNextPage => CurrentPage < PageCount;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to show the first page.
+        /// </summary>
+        public bool ShowFirstPage { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to show the last page.
+        /// </summary>
+        public bool ShowLastPage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pages.
+        /// </summary>
+        public IEnumerable<int> Pages { get; set; }
+    }
+}
---
+++ Web/ViewComponents/PaginationViewComponent.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models.Common;
+
+namespace Web.ViewComponents
+{
+    /// <summary>
+    /// A view component that represents a pagination control.
+    /// </summary>
+    /// <seealso cref="ViewComponent" />
+    public class PaginationViewComponent : ViewComponent
+    {
+        #region Fields
+
+        private const int MaxPages = 5;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Invokes the view component.
+        /// </summary>
+        /// <param name="page">The current page.</param>
+        /// <param name="pageCount">The page count.</param>
+        /// <param name="pageUrlFunc">The function for generating the page URL.</param>
+        public IViewComponentResult Invoke(int page, int pageCount, Func<int, string> pageUrlFunc)
+        {
+            var model = BuildModel(page, pageCount, pageUrlFunc);
+            return View(model);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private PaginationModel BuildModel(int currentPage, int pageCount, Func<int, string> pageUrlFunc)
+        {
+            var (startPage, endPage) = GetPageRange(currentPage, pageCount);
+
+            var model = new PaginationModel
+            {
+                PageUrlFunc = pageUrlFunc,
+                CurrentPage = currentPage,
+                PageCount = pageCount,
+                Pages = Enumerable.Range(startPage, (endPage + 1) - startPage),
+                ShowFirstPage = ShouldShowFirstPage(currentPage),
+                ShowLastPage = ShouldShowLastPage(currentPage, pageCount)
+            };
+
+            return model;
+        }
+
+        private (int StartPage, int EndPage) GetPageRange(int currentPage, int pageCount)
+        {
+            var startPage = 1;
+            var endPage = pageCount;
+
+            if (pageCount > MaxPages)
+            {
+                var maxPagesBeforeCurrent = (int)Math.Floor(MaxPages / 2d);
+                var maxPagesAfterCurrent = (int)Math.Ceiling(MaxPages / 2d) - 1;
+
+                if (currentPage <= maxPagesBeforeCurrent)
+                {
+                    startPage = 1;
+                    endPage = MaxPages;
+                }
+                else if (currentPage + maxPagesAfterCurrent >= pageCount)
+                {
+                    startPage = pageCount - MaxPages + 1;
+                    endPage = pageCount;
+                }
+                else
+                {
+                    startPage = currentPage - maxPagesBeforeCurrent;
+                    endPage = currentPage + maxPagesAfterCurrent;
+                }
+            }
+
+            return (startPage, endPage);
+        }
+
+        private bool ShouldShowFirstPage(int currentPage)
+        {
+            var maxPagesBeforeCurrent = (int)Math.Floor(MaxPages / 2d);
+            return (currentPage - maxPagesBeforeCurrent) > 1;
+        }
+
+        private bool ShouldShowLastPage(int currentPage, int pageCount)
+        {
+            var maxPagesAfterCurrent = (int)Math.Ceiling(MaxPages / 2d) - 1;
+            return (currentPage + maxPagesAfterCurrent) < pageCount;
+        }
+
+        #endregion
+    }
+}
